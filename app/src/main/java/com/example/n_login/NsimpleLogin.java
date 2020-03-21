b725eef26255fe2927d68134d8e823747eddfd46
nth6990 <56827359+nth6990@users.noreply.github.com>
2020-03-21 21:15:13 +0900

    constructed a simple login activity

---
+++ app/src/main/java/com/example/n_login/NsimpleLogin.java
@@ -0,0 +1,50 @@
+package com.example.n_login;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class NsimpleLogin extends AppCompatActivity {
+    TextView simpleLoginPw;
+    Button[] buttons = new Button[9];
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.n_simplelogin);
+
+        simpleLoginPw = findViewById(R.id.simpleLoginPw);
+
+        buttons[0] = (Button) findViewById(R.id.simpleLoginNum1);
+        buttons[1] = (Button) findViewById(R.id.simpleLoginNum2);
+        buttons[2] = (Button) findViewById(R.id.simpleLoginNum3);
+        buttons[3] = (Button) findViewById(R.id.simpleLoginNum4);
+        buttons[4] = (Button) findViewById(R.id.simpleLoginNum5);
+        buttons[5] = (Button) findViewById(R.id.simpleLoginNum6);
+        buttons[6] = (Button) findViewById(R.id.simpleLoginNum7);
+        buttons[7] = (Button) findViewById(R.id.simpleLoginNum8);
+        buttons[8] = (Button) findViewById(R.id.simpleLoginNum9);
+
+        for(int i=0;i<9;i++){
+            final String temp = Integer.toString(i+1);
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    simpleLoginPw.append(temp);
+                    if(simpleLoginPw.getText().length() == 4){
+                        finish();
+                    }
+                }
+            });
+        }
+
+
+    }
+
+}
